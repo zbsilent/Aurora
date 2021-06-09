2e2432e92a367b0bcd869c1a33efc5f5c726ed1c
Reno <zbsilent@gmail.com>
2021-06-09 12:11:57 +0800

    feat: 进行服务注册
    增加pom文件
    增加注解
    配置YML文件

--- cloud-consumer-order8082/src/main/java/com/aurora/springcloud/OrderApplication.java
+++ cloud-consumer-order8082/src/main/java/com/aurora/springcloud/OrderApplication.java
@@ -2,6 +2,7 @@ package com.aurora.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -10,6 +11,7 @@ import org.springframework.web.client.RestTemplate;
  * @date 2021年06月09日 5:07 上午
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderApplication {
   public static void main(String[] args) {
     //
--- cloud-provider-payment8081/src/main/java/com/aurora/springcloud/PaymentApplication.java
+++ cloud-provider-payment8081/src/main/java/com/aurora/springcloud/PaymentApplication.java
@@ -3,6 +3,7 @@ package com.aurora.springcloud;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
@@ -12,6 +13,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @SpringBootApplication
 @MapperScan("com.aurora.springcloud.mapper")
 @EnableTransactionManagement
+@EnableEurekaClient
 public class PaymentApplication {
 
   public static void main(String[] args) {
